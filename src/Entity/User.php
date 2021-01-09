5e16cede7cb2acc424875cb53d6d8aee58a29757
bootta <Bootta@yandex.ru>
2021-01-09 21:51:36 +0300

    added user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -122,7 +122,7 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        // guarantee every user at least has ROLE_USER.
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
