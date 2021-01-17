1797e7081c3cdf56142a0fc79b1007bd4e23844b
bootta <Bootta@yandex.ru>
2021-01-17 16:01:59 +0300

    added user shops and address

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -65,10 +67,24 @@ class User implements UserInterface
      */
     private DateTimeInterface $createdAt;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Address::class, inversedBy="user")
+     * @ORM\JoinTable(name="user_to_addres")
+     */
+    private Collection $addresses;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Shop::class, inversedBy="user")
+     * @ORM\JoinTable(name="user_to_shop")
+     */
+    private Collection $shops;
+
     public function __construct()
     {
         $this->updatedAt = new DateTimeImmutable();
         $this->createdAt = new DateTimeImmutable();
+        $this->addresses = new ArrayCollection();
+        $this->shops = new ArrayCollection();
     }
 
     public function getId(): int
@@ -123,7 +139,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER.
-        $roles[] = 'ROLE_USER';
+        $roles[] = self::ROLE_USER;
 
         return array_unique($roles);
     }
@@ -239,6 +255,60 @@ class User implements UserInterface
     public function setCreatedAt(DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getAddresses(): Collection
+    {
+        return $this->addresses;
+    }
+
+    public function addAddress(Address $address): self
+    {
+        if (!$this->addresses->contains($address)) {
+            $this->addresses[] = $address;
+            $address->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAddress(Address $address): self
+    {
+        if ($this->addresses->removeElement($address)) {
+            $address->removeUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getShops(): Collection
+    {
+        return $this->shops;
+    }
+
+    public function addShop(Shop $shop): self
+    {
+        if (!$this->shops->contains($shop)) {
+            $this->shops[] = $shop;
+            $shop->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeShop(Shop $shop): self
+    {
+        if ($this->shops->removeElement($shop)) {
+            $shop->removeUser($this);
+        }
 
         return $this;
     }
