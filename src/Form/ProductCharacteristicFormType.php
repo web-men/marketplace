215098451a74511b6f4ccc22349dc394b75f458e
bootta <Bootta@yandex.ru>
2021-08-12 13:50:39 +0300

    added multiple characteristics

--- src/Form/ProductCharacteristicFormType.php
+++ src/Form/ProductCharacteristicFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ProductCharacteristic;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,13 +14,23 @@ class ProductCharacteristicFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('add', ButtonType::class, [
+                'attr' => [
+                    'class' => 'characteristic_add',
+                ],
+                'label_html' => true,
+                'label' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle">
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="12" y1="8" x2="12" y2="16"></line>
+            <line x1="8" y1="12" x2="16" y2="12"></line>
+        </svg>',
+            ])
             ->add('name', TextType::class, [
-                'label' => 'characteristic.name'
+                'label' => 'characteristic.name',
             ])
             ->add('value', TextType::class, [
-                'label' => 'characteristic.value'
-            ])
-        ;
+                'label' => 'characteristic.value',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
